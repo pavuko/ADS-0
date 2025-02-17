9b9e5b81542c232870b2ad3697657fde2b7b3c71
pavuko <99765321+pavuko@users.noreply.github.com>
2025-02-17 21:19:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,20 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-
+	int nod = 0;
+	if (a > b) {
+		nod = b;
+		while (!(a % b == 0)) {
+			a = b;
+			nod = a % b;
+		}
+	}
+	else {
+		nod = a;
+		while (!(b % a == 0)) {
+			nod = b % a;
+			b = a;
+		}
+	}
+	return nod;
 }
