46cf29724c8aa492b5e3635ca72aabcc74807bd5
pavuko <99765321+pavuko@users.noreply.github.com>
2025-02-18 20:13:08 +0300

    Update CodStyle alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,24 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-	int nod = 0;
-	if (a > b) {
-		nod = b;
-		while (!(a % b == 0)) {
-			a = b;
-			nod = a % b;
-		}
-	}
-	else {
-		nod = a;
-		while (!(b % a == 0)) {
-			nod = b % a;
-			b = a;
-		}
-	}
-	return nod;
+
+  int nod = 0;
+
+  if (a > b) {
+    nod = b;
+    while (!(a % b == 0)) {
+      nod = a % b;
+      a = b;
+    }
+  }
+
+  else {
+    nod = a;
+    while (!(b % a == 0)) {
+      nod = b % a;
+      b = a;
+    }
+  }
+
+  return nod;
 }
